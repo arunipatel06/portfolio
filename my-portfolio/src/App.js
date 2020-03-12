fe4fdd16d6388e77fadea25944c6853da9d72957
arunipatel06 <arunipatel06@gmail.com>
2020-03-11 22:39:16 -0400

    Setup Routes

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
+// import React from 'react';
 
-function App() {
-  return <div>Hello there</div>;
+// function App() {
+//   return <div>Hello there</div>;
+// }
+
+// export default App;
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import Contact from "./Contact";
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style={{ width: "100%", overflowX: "hidden" }}>
+        <div>
+          <BrowserRouter>
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/contact/:no" component={Contact} />
+              </Switch>
+          </BrowserRouter>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
