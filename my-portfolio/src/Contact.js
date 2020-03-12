86867a61372a84ee9bc14f3f3d26ddaaec161ebb
arunipatel06 <arunipatel06@gmail.com>
2020-03-11 22:39:47 -0400

    Add Contact Component

---
+++ my-portfolio/src/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const Contact = props => {
+  return (
+    <div>
+      <h1>Contact us on {props.match.params.no}</h1>
+    </div>
+  );
+};
+
+export default Contact;
