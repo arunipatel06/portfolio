c31af6f8a0161ff6b3fa9c6f63ee50676c0e8253
arunipatel06 <arunipatel06@gmail.com>
2020-03-17 17:28:23 -0400

    update imports

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -9,6 +9,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import Contact from "./Contact";
+import Progressbar from "./Progressbar";
 class App extends Component {
 
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/contact/:no" component={Contact} />
+                <Route path="/progressBar" component={Progressbar} />
               </Switch>
           </BrowserRouter>
         </div>
