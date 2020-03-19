d606330d8fe6c05c7c6ac453e49ca73c9235c1a1
arunipatel06 <arunipatel06@gmail.com>
2020-03-19 16:11:09 -0400

    create progress bar

--- my-portfolio/src/Progressbar.js
+++ my-portfolio/src/Progressbar.js
@@ -1,85 +1,122 @@
-// import React, { Component } from "react";
-// import ProgressBar from 'react-bootstrap/ProgressBar';
-
-
-
-// class Progressbar extends Component {
-//     let now = 60;
-//     let progressInstance = <ProgressBar now={now} label={`${now}%`} />;
-//     render() {
-//      (progressInstance)
-//      return (
-//               <div>
-//             <div>HELLLO</div>
-//             <div><ProgressBar now={60} /></div>
-
-//             </div>
-//      );
-//     }
-//   }
-
-// const Progressbar = props => {
-//     const now = 60;
-//     const progressInstance = (<div> <ProgressBar now={now} label={`${now}%`} /></div>);
-
-//     return <div>{progressInstance}</div>;
-// };
-
-// export default Progressbar;
-
-import React from 'react';
-import { lighten, makeStyles, withStyles } from '@material-ui/core/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
-
+import React from "react";
+import { lighten, makeStyles, withStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 const BorderLinearProgress = withStyles({
   root: {
-    height: 10,
-    backgroundColor: lighten('#ff6c5c', 0.5),
+    height: 20,
+    backgroundColor: lighten("#007f79", 0.5)
   },
   bar: {
-    borderRadius: 10,
-    backgroundColor: '#ff6c5c',
-  },
+    borderRadius: 20,
+    backgroundColor: "#007f79"
+  }
 })(LinearProgress);
-
-// Inspired by the Facebook spinners.
-const useStylesFacebook = makeStyles({
-  root: {
-    position: 'relative',
-  },
-  top: {
-    color: '#eef3fd',
-  },
-  bottom: {
-    color: '#6798e5',
-    animationDuration: '550ms',
-    position: 'relative',
-    left: 0,
-  },
-});
-
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   margin: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(1)
   },
+
+  aboutheader: {
+    paddingBottom: "9px",
+    margin: "40px 0px 20px",
+    borderBottom: "1px solid rgba(0, 0, 0, 0.25)",
+    textTransform: "uppercase",
+    fontSize: "18px",
+    fontFamily: "freight-sans-pro Helvetica Neue Helvetica Arial sans-serif",
+    fontStyle: "normal",
+    lineHeight: "1.1",
+    color: "Black",
+    // margin: "auto",
+    textAlign: "center"
+  },
+  section4: {
+    padding: "0px 10px"
+  },
+  progresslist: {
+    margin: "20px 0px"
+  },
+
+  progressclass: {
+    fontSize: "14px",
+    fontFamily: "freight-sans-pro Helvetica Neue Helvetica Arial sans-serif",
+    fontStyle: "normal",
+    color: "white",
+    margin: "0px 0px -1px",
+    padding: "10px 15px",
+    backgroundColor: "#7FB0A6"
+  }
 }));
 
-export default function CustomizedProgressBars() {
+export default function Contact() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <BorderLinearProgress
-        className={classes.margin}
-        variant="determinate"
-        color="secondary"
-        value={50}
-      />
+      <Grid container>
+        <Grid item xs={8} style={{ backgroundColor: "red" }}>
+        <h4 className={classes.aboutheader}>Skills Distribution</h4>
+        </Grid>
+        <Grid item xs={4}>
+          <h4 className={classes.aboutheader}>Skills</h4>
+          <div className={classes.progresslist}>
+            <div className={classes.progressclass}>
+              <div>1</div>
+              <BorderLinearProgress
+                className={classes.margin}
+                variant="determinate"
+                color="secondary"
+                value={50}
+              />
+            </div>
+
+            <div className={classes.progressclass}>
+              <div>2</div>
+              <BorderLinearProgress
+                className={classes.margin}
+                variant="determinate"
+                color="secondary"
+                value={50}
+              />
+            </div>
+
+            <div className={classes.progressclass}>
+              <div>3</div>
+              <BorderLinearProgress
+                className={classes.margin}
+                variant="determinate"
+                color="secondary"
+                value={50}
+              />
+            </div>
+
+            <div className={classes.progressclass}>
+              <div>4</div>
+              <BorderLinearProgress
+                className={classes.margin}
+                variant="determinate"
+                color="secondary"
+                value={50}
+              />
+            </div>
+
+            <div className={classes.progressclass}>
+              <div>5</div>
+              <BorderLinearProgress
+                className={classes.margin}
+                variant="determinate"
+                color="secondary"
+                value={50}
+              />
+            </div>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
