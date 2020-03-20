103c84812c2fd5ef04d7326ffe8ef6db1bf09422
arunipatel06 <arunipatel06@gmail.com>
2020-03-20 16:03:01 -0400

    Add route component

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -9,8 +9,6 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import Contact from "./Contact";
-import Progressbar from "./Progressbar";
-import PieChart from "./PieChart";
 import Skills from "./Skills";
 class App extends Component {
 
@@ -21,10 +19,10 @@ class App extends Component {
           <BrowserRouter>
               <Switch>
                 <Route exact path="/" component={HomePage} />
-                <Route path="/contact/:no" component={Contact} />
-                <Route path="/progressbar" component={Progressbar} />
-                <Route path="/piechart" component={PieChart} />
-                <Route path="/skills" component={Skills}/>
+                <Route exact path="/contact/:no" component={Contact} />
+                {/* <Route path="/progressbar" component={Progressbar} />
+                <Route path="/piechart" component={PieChart} /> */}
+                <Route exact path="/skills" component={Skills}/>
 
               </Switch>
           </BrowserRouter>
