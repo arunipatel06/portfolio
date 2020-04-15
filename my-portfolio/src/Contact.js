b42f8aa4802d9eac8d638daa59ddc4115598e275
arunipatel06 <arunipatel06@gmail.com>
2020-04-15 13:14:45 -0400

    update contact details

--- my-portfolio/src/Contact.js
+++ my-portfolio/src/Contact.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Button} from "@material-ui/core";
+import { Grid, Button } from "@material-ui/core";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import MailOutlineRoundedIcon from "@material-ui/icons/MailOutlineRounded";
+// import { Link } from "react-router-dom";
 
 const usestyles = makeStyles(theme => ({
-  
-  icons:{
-    fontSize:'40px',
-    width:'5em',
-    height: '7em',
-    margin:'15px',
-
+  icons: {
+    fontSize: "40px",
+    width: "5em",
+    height: "7em",
+    margin: "15px"
   },
-  div:{
-    margin:'30px',
+  div: {
+    margin: "30px"
   },
   text: {
     margin: "auto",
@@ -26,7 +25,7 @@ const usestyles = makeStyles(theme => ({
     fontWeight: "bold",
     color: "white",
     justifyContent: "center",
-    borderBottom:"3px solid #c3c9c5",
+    borderBottom: "3px solid #c3c9c5"
   },
   body: {
     backgroundColor: "#39373a",
@@ -37,13 +36,19 @@ const usestyles = makeStyles(theme => ({
     alignItems: "center",
     flexDirection: "column"
   },
-  circle:{
-    borderRadius:'60%',
-    height:'60px',
-    width:'60px',
-    padding:'0px',
-    margin:'10px'
+  circle: {
+    borderRadius: "60%",
+    height: "60px",
+    width: "60px",
+    padding: "0px",
+    margin: "10px"
   },
+  anchor: {
+    color: "#39373a",
+    margin: "0px",
+    padding: "0px",
+    fontSize: "40px"
+  }
 }));
 const Contact = props => {
   const classes = usestyles();
@@ -57,21 +62,52 @@ const Contact = props => {
           </div>
           <div className={classes.div}>
             <Button variant="contained" className={classes.circle}>
-              <LinkedInIcon  disableElevation={true} style={{ fontSize:"40px" , margin:'0px' }}/>
+              <a
+                href="https://www.linkedin.com/in/aruni-patel-541b94164/"
+                className={classes.anchor}
+              >
+                <LinkedInIcon
+                  disableElevation={true}
+                  style={{ fontSize: "40px", margin: "0px" }}
+                />
+              </a>
             </Button>
 
             <Button variant="contained" className={classes.circle}>
-              <MailOutlineRoundedIcon  disableElevation={true} style={{ fontSize:"40px" , margin:'0px' }}/>
+              <a
+                href="https://www.instagram.com/a_r_u_95/"
+                className={classes.anchor}
+              >
+                <MailOutlineRoundedIcon
+                  disableElevation={true}
+                  style={{ fontSize: "40px", margin: "0px" }}
+                />
+              </a>
             </Button>
 
             <Button variant="contained" className={classes.circle}>
-              <GitHubIcon  disableElevation={true} style={{ fontSize:"40px" , margin:'0px' }}/>
+              <a
+                href="https://github.com/arunipatel06"
+                className={classes.anchor}
+              >
+                <GitHubIcon
+                  disableElevation={true}
+                  style={{ fontSize: "40px", margin: "0px" }}
+                />
+              </a>
             </Button>
 
             <Button variant="contained" className={classes.circle}>
-              <InstagramIcon  disableElevation={true} style={{ fontSize:"40px" , margin:'0px' }}/>
+              <a
+                href="https://www.instagram.com/a_r_u_95/"
+                className={classes.anchor}
+              >
+                <InstagramIcon
+                  disableElevation={true}
+                  style={{ fontSize: "40px", margin: "0px" }}
+                />
+              </a>
             </Button>
-            
           </div>
         </section>
       </Grid>
