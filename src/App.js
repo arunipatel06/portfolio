45a22c3dc40a13918a06627df01d41df16ec2772
arunipatel06 <arunipatel06@gmail.com>
2020-04-21 21:35:54 -0400

    Route Hexagon component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-// import React from 'react';
-
-// function App() {
-//   return <div>Hello there</div>;
-// }
-
-// export default App;
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import Contact from "./Contact";
 import Skills from "./Skills";
 import Education from "./Education";
-import EducationTimeline from "./EducationTimeline";
+// import EducationTimeline from "./EducationTimeline";
+import Hexx from "./Hexx";
 class App extends Component {
 
   render() {
@@ -24,7 +18,9 @@ class App extends Component {
                 <Route exact path="/contact" component={Contact} />
                 <Route exact path="/skills" component={Skills}/>
                 <Route exact path="/education" component={Education}/>
-                <Route exact path="/educationtimeline" component={EducationTimeline}/>
+                {/* <Route exact path="/educationtimeline" component={EducationTimeline}/> */}
+                <Route exact path="/hexskill" component={Hexx}/>
+
               </Switch>
           </BrowserRouter>
         </div>
