db4ba17634c3b740cbc81100edb9b7ec9b3c697d
arunipatel06 <arunipatel06@gmail.com>
2020-04-21 21:32:01 -0400

    Remove vertical timeline

--- src/Education.js
+++ src/Education.js
@@ -1,12 +1,7 @@
 import React from "react";
 import { Grid, Divider, Typography , Card, CardContent} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import LensRoundedIcon from "@material-ui/icons/LensRounded";
 import NewNavbar from "./NewNavbar";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import SchoolIcon from '@material-ui/icons/School';
 
@@ -14,7 +9,7 @@ const usestyles = makeStyles(theme => ({
   page: {
     height: "100vh",
     // width: "100%",
-    backgroundColor: "white",
+    backgroundColor: "grey",
     padding: "35px 20px"
   },
   title: {
@@ -85,41 +80,7 @@ const usestyles = makeStyles(theme => ({
 const Education = props => {
   const classes = usestyles();
 
-  const education = (
-    <div style={{backgroundColor:"grey"}}>
-      <VerticalTimeline layout={'1-column'}>
-      <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="April 2013"
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-    icon={<SchoolIcon />}
-    // contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    contentStyle={{ borderTop: '4px solid  rgb(233, 30, 99)' }}
-  >
-    <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-    <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-    <p>
-      Strategy, Social Media
-    </p>
-  </VerticalTimelineElement >
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2002 - 2006"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-          icon={<SchoolIcon />}
-          contentStyle={{ borderTop: '4px solid  rgb(233, 30, 99)' }}
-        >
-          <h3 className="vertical-timeline-element-title" >
-            Bachelor of Science in Interactive Digital Media Visual Imaging
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Bachelor Degree
-          </h4>
-          <p>Creative Direction, Visual Design</p>
-        </VerticalTimelineElement>
-      </VerticalTimeline>
-    </div>
-  );
+  
   const masterEducation = (
     <Card className={classes.root}>
       <CardContent>
@@ -161,13 +122,12 @@ const Education = props => {
         <Grid container spacing={2}>
           <Grid item xs={12}>
           <Typography className={classes.title}>Education</Typography>
-          {education}
           </Grid>
           
           <Grid item xs={3}>
             <div className={classes.heading}>Masters </div>
             <div className={classes.bullets}>
-              <LensRoundedIcon className={classes.icons} />
+              <SchoolIcon className={classes.icons} />
               <Divider style={{height:"3px", width:"180px",  backgroundColor: "#fcc93a",}}/>
             </div>
           </Grid>
@@ -181,7 +141,7 @@ const Education = props => {
           <Grid item xs={3}>
             <div className={classes.heading}>Bachelors </div>
             <div className={classes.bullets}>
-              <LensRoundedIcon className={classes.icons} />
+              <SchoolIcon className={classes.icons} />
               <Divider style={{height:"3px", width:"180px",  backgroundColor: "#fcc93a",}}/>
             </div>
           </Grid>
