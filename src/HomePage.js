473cd4fdb6c04ef2f10882f4878005f59d045440
arunipatel06 <arunipatel06@gmail.com>
2020-04-23 16:53:04 -0400

    Fix Styling of homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,59 +1,82 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Avatar} from "@material-ui/core";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import Background from "./assests/Homepage3.jpg";
-import AvatarImage from "./assests/Git.jpg";
-import "typeface-roboto";
-import "./HomePage.css";
-import Skills from "./Skills";
-import Contact from "./Contact";
-import Education from "./Education";
-import AboutMe from "./AboutMe";
-
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Avatar, Typography, Divider } from '@material-ui/core';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import Background from './assests/Homepage.jpg';
+import AvatarImage from './assests/Git.jpg';
+import 'typeface-roboto';
+import './HomePage.css';
+import Skills from './Skills';
+import Contact from './Contact';
+import Education from './Education';
+import NewNavbar from './Navbar';
 
 const useStyles = makeStyles((theme) => ({
-  homeContent: {
-    fontFamily: "freight-sans-pro Helvetica Neue Helvetica Arial sans-serif",
-    fontStyle: "normal",
-    fontSize: "40px",
-    fontWeight: "bold",
-    color: "white",
-    marginBottom: "0px",
-    textAlign: "center",
-    textTransform: "uppercase",
-  },
-  typo: {
-    textAlign: "center",
-    color: "white",
-    fontSize: "45px",
-    lineHeight: "50px",
-    textTransform: "uppercase",
-    fontWeight: "bold",
-    fontFamily: "freight-sans-pro Helvetica Neue Helvetica Arial sans-serif",
-    margin: "0px auto",
+  //--------------------
+  root: {
+    overflow: 'hidden',
   },
   avatar: {
-    height: theme.spacing(18),
-    width: theme.spacing(18),
-    border: "3px solid white",
-    margin: "250px auto 0px",
+    height: theme.spacing(20),
+    width: theme.spacing(20),
+    border: '3px solid white',
+    margin: '0px auto 20px',
   },
-
+  upperTitle: {
+    fontFamily: '"Raleway", sans-serif',
+    fontStyle: 'normal',
+    fontSize: '22px',
+    lineHeight: '30px',
+    fontWeight: 600,
+    color: '#f0f2f5',
+    margin: 'auto',
+    textAlign: 'center',
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '24px',
+      lineHeight: '30px',
+    },
+  },
+  titleName: {
+    fontFamily: '"Raleway", sans-serif',
+    fontStyle: 'normal',
+    fontSize: '50px',
+    lineHeight: '65px',
+    fontWeight: 900,
+    color: '#f0f2f5',
+    margin: 'auto',
+    textAlign: 'center',
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '70px',
+      lineHeight: '85px',
+    },
+  },
+  subTitleName: {
+    fontFamily: '"Mada", sans-serif',
+    fontStyle: 'normal',
+    fontSize: '24px',
+    lineHeight: '35px',
+    fontWeight: 'normal',
+    color: '#1b242f',
+    margin: 'auto',
+    textAlign: 'center',
+  },
+  typo: {
+    textAlign: 'center',
+    color: '#E31B6D',
+  },
   homeImage: {
-    height: "100vh",
-    width: "100%",
-    textAlign: "center",
+    height: '100vh',
+    width: '100%',
+    textAlign: 'center',
     backgroundImage: `url(${Background})`,
-    backgroundRepeat: "no-repeat",
-    background: 'linear-gradient(grey)',
-    backgroundSize: "cover",
-    display: "flex",
-    // justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    // margin: "0px",
-    padding: "0px",
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    padding: '0px',
   },
 }));
 
@@ -61,40 +84,52 @@ const HomePage = (props) => {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
       <Grid item xs={12}>
         <section className={classes.homeImage}>
           <div>
             <Avatar src={AvatarImage} className={classes.avatar} />
-            <p className={classes.homeContent} style={{ fontSize: "25px" }}>
-              My Name is
-            </p>
-            <p className={classes.typo}>Aruni Patel</p>
+            <Typography varient="h3" className={classes.upperTitle}>
+              MY NAME IS
+            </Typography>
+            <Typography varient="h3" className={classes.titleName}>
+              ARUNI PATEL
+            </Typography>
+            <Divider style={{ height: '1px', backgroundColor: '#f0f2f5' }} />
+            <Typography
+              variant="h5"
+              className={classes.upperTitle}
+              style={{ marginTop: '10px' }}
+            >
+              DEVELOPER, CODER
+            </Typography>
             <div
               className="homeWork"
-              style={{ borderRadius:"25px" ,"&:hover": { backgroundColor: "#555" } }}
+              style={{
+                borderRadius: '25px',
+                margin: '20px auto 0px',
+                '&:hover': { backgroundColor: '#555' },
+              }}
             >
               View my work
               <ArrowForwardIcon
                 className="Arrow"
                 style={{
-                  position: "absolute",
-                  color: "white",
-                  fontSize: "30",
-                  right: "25px",
+                  position: 'absolute',
+                  fontSize: '20',
+                  right: '35px',
+                  top: '8px',
                 }}
               />
             </div>
           </div>
         </section>
       </Grid>
-      <div><AboutMe/></div>
+      <NewNavbar />
       <div>
         <Skills />
       </div>
-      <div
-        style={{ display: "flex", justifyContent: "center", height: "100vh" }}
-      >
+      <div style={{ justifyContent: 'center', height: '100vh' }}>
         <Education />
       </div>
       <div>
