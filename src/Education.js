e4e42e742ce632e6a01b41511b93da3b48253121
arunipatel06 <arunipatel06@gmail.com>
2020-04-23 16:49:30 -0400

    Change education component

--- src/Education.js
+++ src/Education.js
@@ -1,158 +1,78 @@
-import React from "react";
-import { Grid, Divider, Typography , Card, CardContent} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import NewNavbar from "./NewNavbar";
-import "react-vertical-timeline-component/style.min.css";
-import SchoolIcon from '@material-ui/icons/School';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Timeline } from 'antd';
+import { Grid, Typography, Divider } from '@material-ui/core';
+import 'antd/dist/antd.css';
+import './Education.css';
 
-const usestyles = makeStyles(theme => ({
-  page: {
-    height: "100vh",
-    // width: "100%",
-    backgroundColor: "grey",
-    padding: "35px 20px"
-  },
+//Icons
+import { ClockCircleOutlined } from '@ant-design/icons';
+
+const usestyles = makeStyles((theme) => ({
   title: {
-    textAlign: "center",
-    fontFamily: "freight-sans-pro Helvetica Neue Helvetica Arial sans-serif",
-    textTransform: "uppercase",
-    fontSize: "35px",
-    fontWeight: 600,
-    color: "grey",
-    margin: "80px 0px 25px 0px",
+    textAlign: 'left',
+    fontFamily: '"Manrope",sans-serif',
+    fontSize: '22px',
+    lineHeight: '30px',
+    fontWeight: 'normal',
+    color: 'grey',
   },
-  border: {
-    borderLeft: "5px solid #fcc93a",
-    margin: "0px 0px 0px 20px ",
-    height: "3"
-    // padding:"30px"
-  },
-  icons: {
-    // position: "absolute",
-    color: "#fcc93a",
-    fontSize: "25px",
-    // margin: "0px 0px 40px 0px"
-    // border: "3px solid grey",
-    // borderRadius: "50%"
-
-  },
-  bullets: {
-    display:"flex",
-    flexDirection:"row",
-    justifyContent:"left",
-    alignItems:"center"
-  },
-
-  contentContainer: {
-    paddingBottom:"25px"
+  contentBody: {
+    paddingBottom: '25px',
   },
   heading: {
-    textAlign: "center",
-    fontSize: "20px",
-    fontFamily: "freight-sans-pro Helvetica Neue Helvetica Arial sans-serif",
-    overflow:"hidden"
+    textAlign: 'left',
+    fontFamily: '"Manrope",sans-serif',
+    fontSize: '20px',
+    lineHeight: '30px',
+    fontWeight: 600,
+    color: 'grey',
   },
-  year: {
-    fontFamily: "Helvetica Neue Helvetica Arial",
-    fontSize: "15px",
-    color: "grey",
-    fontStyle: "bold"
+  content: {
+    textAlign: 'left',
+    fontFamily: '"Manrope",sans-serif',
+    fontSize: '18px',
+    lineHeight: '25px',
+    fontWeight: 'normal',
+    color: 'grey',
   },
-
-  degree: {
-    fontStyle: "italic",
-    margin: "10px 0px 10px 15px",
-    fontSize: "15px",
-    fontFamily: "sans-serif"
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  cardTitle: {
-    fontSize: "18px",
-  },
-  pos: {
-    marginBottom: 12,
+  icons: {
+    color: '#fcc93a',
+    fontSize: '25px',
   },
 }));
-const Education = props => {
+
+const Education = (props) => {
   const classes = usestyles();
-
-  
-  const masterEducation = (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography>
-          Concordia University
-        </Typography>
-        <Typography  className={classes.cardTitle} color="textSecondary" gutterBottom>
-            Masters In Software Engineering
-        </Typography>
-
-        {/* <Typography className={classes.pos} variant="body2" component="p" color="textSecondary">
-          Major Courses: Sofware Design Methodologies, Software Architecture
-        </Typography> */}
-      </CardContent>
-    </Card>
-  );
-
-  const BachelorEducation = (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography>
-          Gujarat Technological University
-        </Typography>
-        <Typography  className={classes.cardTitle} color="textSecondary" gutterBottom>
-            Bachelors in Computer Engineering
-        </Typography>
-
-        {/* <Typography className={classes.pos} variant="body2" component="p" color="textSecondary">
-        </Typography> */}
-      </CardContent>
-    </Card>
-  );
-
-
-
+  const mode = 'left';
   return (
-    <div style={{overflow:'hidden' ,maxWidth:'70%'}} >
-      <div><NewNavbar /></div>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-          <Typography className={classes.title}>Education</Typography>
-          </Grid>
-          
-          <Grid item xs={3}>
-            <div className={classes.heading}>Masters </div>
-            <div className={classes.bullets}>
-              <SchoolIcon className={classes.icons} />
-              <Divider style={{height:"3px", width:"180px",  backgroundColor: "#fcc93a",}}/>
-            </div>
-          </Grid>
-
-          <Grid item xs={9}>
-            <div className={classes.contentContainer}>
-              {masterEducation}
-            </div>
-          </Grid>
-
-          <Grid item xs={3}>
-            <div className={classes.heading}>Bachelors </div>
-            <div className={classes.bullets}>
-              <SchoolIcon className={classes.icons} />
-              <Divider style={{height:"3px", width:"180px",  backgroundColor: "#fcc93a",}}/>
-            </div>
-          </Grid>
-
-          <Grid item xs={9}>
-            <div className={classes.contentContainer}>
-              {BachelorEducation}
-            </div>
-          </Grid>
-        </Grid>
-
+    <div style={{ width: '100%', marginTop: '200px' }}>
+      <Timeline mode={mode}>
+        <Timeline.Item
+          label="2015-2017"
+          dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}
+          color="red"
+        >
+          <Typography className={classes.heading}>
+            Solve initial network problems
+          </Typography>
+          <Typography className={classes.content}>
+            Solve initial network problems
+          </Typography>
+        </Timeline.Item>
+        <Timeline.Item
+          label="2015-2017"
+          dot={<ClockCircleOutlined style={{ fontSize: '26px' }} />}
+          color="red"
+        >
+          <Typography className={classes.heading}>
+            Solve initial network problems
+          </Typography>
+          <Typography className={classes.content}>
+            Solve initial network problems
+          </Typography>
+        </Timeline.Item>
+      </Timeline>
     </div>
   );
 };
