ee1555130c281d071428a512e0768e0d633d3143
arunipatel06 <arunipatel06@gmail.com>
2020-04-30 15:54:02 -0400

    Add Resume

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,36 +1,45 @@
-import React from "react";
-import { Grid, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { Grid, Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import GetAppIcon from '@material-ui/icons/GetApp';
 
 const useStyles = makeStyles((theme) => ({
   title: {
-    textAlign: "center",
+    textAlign: 'center',
     fontFamily: '"Manrope", sans-serif',
-    fontSize: "30px",
-    lineHeight: "35px",
-    display: "flex",
-    color: "#260859",
-    justifyContent: "center",
-    margin: "60px 50px 35px 50px",
+    fontSize: '38px',
+    lineHeight: '35px',
+    display: 'flex',
+    color: '#260859',
+    justifyContent: 'center',
+    margin: '0px 50px',
   },
 
   aboutme: {
     fontWeight: 100,
     fontFamily: '"Manrope", sans-serif',
-    color: "#260859",
-    fontSize: "20px",
-    lineHeight: "30px",
-    marginBottom: "100px",
-    justifyContent: "center",
-    width:"35%",
-    textAlign:"center",
-    margin:"0px auto"
+    color: '#260859',
+    fontSize: '20px',
+    lineHeight: '30px',
+    // marginBottom: '140px',
+    justifyContent: 'center',
+    width: '35%',
+    textAlign: 'center',
+    margin: '30px auto',
   },
 
   content: {
-    backgroundColor: "#f2f2f2",
-    textAlign: "center",
-    justifyContent: "center",
+    backgroundColor: '#f2f2f2',
+    textAlign: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    borderRadius: '25%',
+    // height: '60px',
+    // width: '60px',
+    padding: '0px',
+    margin: '10px',
+    backgroundColor: '#8c8c8c',
   },
 }));
 
@@ -43,18 +52,35 @@ const AboutMe = (props) => {
         <Grid item xs={12} className={classes.content}>
           <div
             style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              margin: '150px auto',
             }}
           >
             <Typography className={classes.title}>Hello! I'm Aruni</Typography>
             <Typography className={classes.aboutme}>
-              I am a recent University Graduate living in Toronto. I have
-              1+ years of experience in development and programming field. I am efficient and
-              enthusiastic to collaborate with team by means of strong
-              commitment.
+              I am a recent University Graduate living in Toronto. I have 1+
+              years of experience in development and programming field. I am
+              efficient and enthusiastic to collaborate with team by means of
+              strong commitment.
             </Typography>
+            <div>
+              <Button
+                variant="contained"
+                style={{ '&:hover': { backgroundColor: '#555' } }}
+              >
+                <GetAppIcon
+                  disableElevation={true}
+                  style={{ fontSize: '30px', margin: '0px' }}
+                />
+                <Typography>Resume</Typography>
+                <a
+                  href="https://www.instagram.com/a_r_u_95/"
+                  className={classes.anchor}
+                ></a>
+              </Button>
+            </div>
           </div>
         </Grid>
       </Grid>
