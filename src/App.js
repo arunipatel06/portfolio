0412c4e27995d2975cc6ffed3cfc23700cbc97fb
arunipatel06 <arunipatel06@gmail.com>
2020-05-05 14:42:25 -0400

    Update application setting

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React, { Component } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./HomePage";
-import Contact from "./Contact";
-import Skills from "./Skills";
-import Education from "./Education";
-import AboutMe from "./AboutMe";
+
 class App extends Component {
 
   render() {
     return (
       <div style={{ width: "100%", overflowX: "hidden" }}>
         <div>
-          <BrowserRouter>
-              <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route exact path="/contact" component={Contact} />
-                <Route exact path="/aboutme" component={AboutMe} />
-                <Route exact path="/skills" component={Skills}/>
-                <Route exact path="/education" component={Education}/>
-                {/* <Route exact path="/educationtimeline" component={EducationTimeline}/> */}
-                {/* <Route exact path="/hexskill" component={Hexx}/>
-                <Route exact path="/Hexaskill" component={HexSkills}/> */}
-
-
-              </Switch>
-          </BrowserRouter>
+          <HomePage />
         </div>
       </div>
     );
