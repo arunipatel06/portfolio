fd11bb10f3f9cd7baab005ba9027285a40b9eb6b
arunipatel06 <arunipatel06@gmail.com>
2020-05-05 09:44:32 -0400

    Update style and resume button

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,45 +1,67 @@
-import React from 'react';
-import { Grid, Typography, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import GetAppIcon from '@material-ui/icons/GetApp';
+import React from "react";
+import { Grid, Typography, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import GetAppIcon from "@material-ui/icons/GetApp";
 
 const useStyles = makeStyles((theme) => ({
   title: {
-    textAlign: 'center',
+    textAlign: "center",
     fontFamily: '"Manrope", sans-serif',
-    fontSize: '38px',
-    lineHeight: '35px',
-    display: 'flex',
-    color: '#260859',
-    justifyContent: 'center',
-    margin: '0px 50px',
+    fontSize: "38px",
+    lineHeight: "35px",
+    display: "flex",
+    color: "#260859",
+    justifyContent: "center",
+    margin: "0px 50px",
   },
 
   aboutme: {
     fontWeight: 100,
     fontFamily: '"Manrope", sans-serif',
-    color: '#260859',
-    fontSize: '20px',
-    lineHeight: '30px',
-    // marginBottom: '140px',
-    justifyContent: 'center',
-    width: '35%',
-    textAlign: 'center',
-    margin: '30px auto',
+    color: "#260859",
+    fontSize: "20px",
+    lineHeight: "30px",
+    justifyContent: "center",
+    width: "80%",
+    textAlign: "center",
+    margin: "30px auto",
+    [theme.breakpoints.up("md")]: {
+      width: "60%",
+    },
+    [theme.breakpoints.up("lg")]: {
+      width: "35%",
+    },
   },
 
   content: {
-    backgroundColor: '#f2f2f2',
-    textAlign: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f2f2f2",
+    textAlign: "center",
+    justifyContent: "center",
   },
   button: {
-    borderRadius: '25%',
-    // height: '60px',
-    // width: '60px',
-    padding: '0px',
-    margin: '10px',
-    backgroundColor: '#8c8c8c',
+    borderRadius: "25%",
+    padding: "0px",
+    margin: "10px",
+    backgroundColor: "#8c8c8c",
+  },
+  cardButton: {
+    fontFamily: "sans-serif",
+    margin: theme.spacing(1),
+    padding: "2px 27px",
+    textTransform: "none",
+    fontSize: "19px",
+    borderRadius: "25px",
+    color: "#260859",
+  },
+  aboutMeBody: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    margin: "50px auto",
+    [theme.breakpoints.up("lg")]: {
+      margin: "150px auto",
+    },
+    
   },
 }));
 
@@ -50,14 +72,7 @@ const AboutMe = (props) => {
     <div>
       <Grid container>
         <Grid item xs={12} className={classes.content}>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              margin: '150px auto',
-            }}
-          >
+          <div className={classes.aboutMeBody}>
             <Typography className={classes.title}>Hello! I'm Aruni</Typography>
             <Typography className={classes.aboutme}>
               I am a recent University Graduate living in Toronto. I have 1+
@@ -67,18 +82,17 @@ const AboutMe = (props) => {
             </Typography>
             <div>
               <Button
-                variant="contained"
-                style={{ '&:hover': { backgroundColor: '#555' } }}
+                variant="outlined"
+                startIcon={<GetAppIcon />}
+                className={classes.cardButton}
+                size="large"
+                onClick={() =>
+                  window.open(
+                    "https://drive.google.com/open?id=1cowb0MoU3mPuY4coQfULbrucjXzyLqc-"
+                  )
+                }
               >
-                <GetAppIcon
-                  disableElevation={true}
-                  style={{ fontSize: '30px', margin: '0px' }}
-                />
-                <Typography>Resume</Typography>
-                <a
-                  href="https://www.instagram.com/a_r_u_95/"
-                  className={classes.anchor}
-                ></a>
+                Resume
               </Button>
             </div>
           </div>
